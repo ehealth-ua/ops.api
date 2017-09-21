2801ce0a69c541e4b192ea97522476f4b3af8a12
gmile <iamexile@gmail.com>
2017-09-21 15:08:06 +0300

    Modify test.rb to user two separate DBs

--- test.rb
+++ test.rb
@@ -5,18 +5,23 @@ template_file = File.read('output_nice.json')
 template = JSON.parse(template_file)
 
 conn = PG.connect(dbname: 'ops_dev')
+conn_seeds = PG.connect(dbname: 'ops_seeds_dev')
 
-DAYS = 14
-PER_DAY = 100..200
+DAYS = 7
+PER_DAY = 1..1
 
-puts "Preparing the DB..."
+puts "Preparing DBs..."
 
-conn.exec("
-          CREATE EXTENSION IF NOT EXISTS pgcrypto;
+conn_seeds.exec("
+  CREATE EXTENSION IF NOT EXISTS pgcrypto;
   DELETE FROM seeds;
-  DELETE FROM declarations;
 
   INSERT INTO seeds (hash, debug, inserted_at) VALUES (digest('Слава Україні!', 'sha512'), 'Слава Україні!', '2014-01-01 23:59:59');
+")
+
+conn.exec("
+  CREATE EXTENSION IF NOT EXISTS pgcrypto;
+  DELETE FROM declarations;
 ")
 
 puts "Generating data, for #{DAYS} days, #{PER_DAY} declarations every day..."
@@ -25,7 +30,7 @@ DAYS.times do |day|
   yesterday = (Date.new(2014, 1, 1) + day).to_s
   today = (Date.new(2014, 1, 1) + day + 1).to_s
 
-  seed = conn.exec("SELECT hash FROM seeds ORDER BY inserted_at DESC LIMIT 1").map { |row| row["hash"] }[0]
+  seed = conn_seeds.exec("SELECT hash FROM seeds ORDER BY inserted_at DESC LIMIT 1").map { |row| row["hash"] }[0]
   samples = PER_DAY.to_a.sample
 
   samples.times do |_declaration|
@@ -47,7 +52,6 @@ DAYS.times do |day|
         inserted_at,
         updated_at,
         declaration_request_id,
-        signed_data,
         seed
       ) VALUES (
         uuid_generate_v4(),
@@ -66,13 +70,12 @@ DAYS.times do |day|
         '#{today}',
         '#{today}',
         '3BA18EA0-09A7-4D5D-9330-029E02DD29AB',
-        '#{template_file}',
         '#{seed}'
       )"
     )
   end
 
-  conn.exec("
+  new_seed = conn.exec("
     WITH concat AS (
       SELECT
         ARRAY_TO_STRING(ARRAY_AGG(
@@ -89,67 +92,69 @@ DAYS.times do |day|
             legal_entity_id,
             inserted_at,
             declaration_request_id,
-            signed_data,
             seed
           ) ORDER BY id ASC
         ), '') AS value FROM declarations WHERE DATE(inserted_at) = '#{today}'
     )
-    INSERT INTO seeds (hash, debug, inserted_at)
-    SELECT digest(value, 'sha512'), value, '#{today} 23:59:59' FROM concat;
-  ")
+    SELECT digest(value, 'sha512') as new_seed, value FROM concat;
+  ")[0]
 
-  puts "Day #{today}: generated #{samples} declarations."
+  new_hash = new_seed["new_seed"]
+  new_value = new_seed["value"]
+
+  seed = conn_seeds.exec("INSERT INTO seeds (hash, debug, inserted_at) VALUES (digest('#{new_seed}', 'sha512'), '#{new_value}', '#{today} 23:59:59') returning hash")[0]['hash']
+
+  puts "Day #{today}: generated #{samples} declarations: "
+  puts "  seed: #{seed}"
+  puts "  value: #{new_value}"
 end
 
-puts "Verifying..."
+puts "
+Verifying...
 
-sql = "
-   SELECT seeds.day,
-          declarations.count as count,
-          seeds.existing_hash = declarations.calculated_hash AS day_is_valid
-     FROM (
-            SELECT DISTINCT DATE(inserted_at) as day
-              FROM seeds
-          ) AS days
-     JOIN (
-              SELECT DATE(inserted_at) AS day,
-                     digest(array_to_string(array_agg(
-                       concat(
-                         id,
-                         employee_id,
-                         start_date,
-                         end_date,
-                         signed_at,
-                         created_by,
-                         is_active,
-                         scope,
-                         division_id,
-                         legal_entity_id,
-                         inserted_at,
-                         declaration_request_id,
-                         signed_data,
-                         seed
-                       ) ORDER BY id ASC
-                     ), ''), 'sha512')::bytea AS calculated_hash,
-                     count(1) as count
-                FROM declarations
-            GROUP BY DATE(inserted_at)
-          ) AS declarations ON declarations.day = days.day
-     JOIN (
-            SELECT DATE(inserted_at) AS day,
-                   hash AS existing_hash
-              FROM seeds
-          ) AS seeds ON seeds.day = days.day
 "
 
-result = conn.exec(sql)
+conn.exec("SELECT DISTINCT date(inserted_at) AS day FROM declarations ORDER BY day;").each do |row|
+  day = row['day']
 
-puts result.map { |r| "On day #{r["day"]} there was #{r["count"]} declarations. Day is valid?: #{r["day_is_valid"] == 't' ? 'true' : 'false'}" }.join("\n")
+  new_seed = conn.exec("
+    WITH concat AS (
+      SELECT
+        ARRAY_TO_STRING(ARRAY_AGG(
+          CONCAT(
+            id,
+            employee_id,
+            start_date,
+            end_date,
+            signed_at,
+            created_by,
+            is_active,
+            scope,
+            division_id,
+            legal_entity_id,
+            inserted_at,
+            declaration_request_id,
+            seed
+          ) ORDER BY id ASC
+        ), '') AS value FROM declarations WHERE DATE(inserted_at) = '#{day}'
+    )
+    SELECT digest(value, 'sha512') as new_seed, value FROM concat;
+  ")[0]
 
-failed_days = result.select { |item| item["day_is_valid"] != 't' }
+  new_hash = new_seed["new_seed"]
+  new_value = new_seed["value"]
 
-if failed_days.size > 0
-  puts "The following days failed verification: #{failed_days.map { |day| day['day'] }}"
-else
-  puts "All good!"
+  existing_hash = conn_seeds.exec("SELECT hash FROM seeds WHERE date(inserted_at) = '#{day}'").map { |row| row["hash"] }[0]
+
+  if new_hash == existing_hash
+    puts "Day #{day} vas verified. It's correct!"
+  else
+    puts "Day #{day} vas verified. It's not correct!"
+    puts "  - recalculated hash: #{new_hash}"
+    puts "  - existing hash: #{existing_hash}"
+    puts "    - #{new_value}"
+  end
 end
+
+# 9d62e831-e727-4b58-87db-01273e110d4213d3af77-82e6-4fcb-a2b3-4451d9e685e32017-07-142017-07-152017-09-21 15:03:01.675414ccc6c85b-c4dc-43fc-8e75-ba9b855ea597tfamily_doctora7007627-cb29-4159-b813-68cd51bb3d989c81824b-bc13-4d07-bc76-b069e2a2876b2014-01-08 00:00:003ba18ea0-09a7-4d5d-9330-029e02dd29ab\x5fbed87f9a67607b37e6297e47ed29e74a0a38a516e3bb2b549dd0d92dbd3910b3e5909c15fbd8aea05ac26fbda7da1efaef0da5be1700176980aaefa84dabc9
+# 9d62e831-e727-4b58-87db-01273e110d4213d3af77-82e6-4fcb-a2b3-4451d9e685e32017-07-142017-07-152017-09-21 15:03:01.675414ccc6c85b-c4dc-43fc-8e75-ba9b855ea597tfamily_doctora7007627-cb29-4159-b813-68cd51bb3d989c81824b-bc13-4d07-bc76-b069e2a2876b2014-01-08 00:00:003ba18ea0-09a7-4d5d-9330-029e02dd29ab\x5fbed87f9a67607b37e6297e47ed29e74a0a38a516e3bb2b549dd0d92dbd3910b3e5909c15fbd8aea05ac26fbda7da1efaef0da5be1700176980aaefa84dabc9
