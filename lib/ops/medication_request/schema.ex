d4a3e3630dfa4887251684d3a9e488836b547b1e
Alex Kovalevych <alexkovalevych@gmail.com>
2017-09-26 15:08:18 +0300

    fix medication_request schema

--- lib/ops/medication_request/schema.ex
+++ lib/ops/medication_request/schema.ex
@@ -25,13 +25,11 @@ defmodule OPS.MedicationRequest.Schema do
     field :employee_id, Ecto.UUID
     field :medication_id, Ecto.UUID
     field :medication_qty, :float
-    field :note, :map
-    field :dosage_instuction, :map
     field :status, :string
     field :is_active, :boolean
-    field :recalled_at, :date
-    field :recalled_by, Ecto.UUID
-    field :recall_reason, :string
+    field :rejected_at, :date
+    field :rejected_by, Ecto.UUID
+    field :reject_reason, :string
     field :medication_request_requests_id, Ecto.UUID
     field :medical_program_id, Ecto.UUID
     field :inserted_by, Ecto.UUID
