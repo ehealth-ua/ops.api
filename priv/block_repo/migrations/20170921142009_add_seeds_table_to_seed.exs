a7cf22aec00767c75d70049f43490304b19628d4
gmile <iamexile@gmail.com>
2017-09-26 15:23:39 +0300

    Squash migrations

--- priv/block_repo/migrations/20170921142009_add_seeds_table_to_seed.exs
+++ priv/block_repo/migrations/20170921142009_add_seeds_table_to_seed.exs
@@ -1,12 +1,21 @@
 defmodule OPS.BlockRepo.Migrations.AddSeedsTableToSeed do
   use Ecto.Migration
 
-  def change do
+  def up do
     create table(:blocks, primary_key: false) do
       add :hash, :string, null: false
-      add :day, :date, null: false
+      add :block_start, :utc_datetime, null: false
+      add :block_end, :utc_datetime, null: false
 
       timestamps(updated_at: false, type: :utc_datetime)
     end
+
+    execute("CREATE EXTENSION IF NOT EXISTS pgcrypto")
+  end
+
+  def down do
+    drop table(:blocks)
+
+    execute("DROP EXTENSION IF EXISTS pgcrypto")
   end
 end
--- priv/block_repo/migrations/20170921153126_add_crypto_ext.exs
+++
@@ -1,7 +0,0 @@
-defmodule OPS.BlockRepo.Migrations.AddCryptoExt do
-  use Ecto.Migration
-
-  def change do
-    execute("CREATE EXTENSION IF NOT EXISTS pgcrypto")
-  end
-end
