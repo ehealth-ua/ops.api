b38c811d2588cc06a2004569600447368f6da583
gmile <iamexile@gmail.com>
2017-09-26 21:49:07 +0300

    Fix var names

--- lib/ops/tasks.ex
+++ lib/ops/tasks.ex
@@ -5,19 +5,19 @@ defmodule :ops_tasks do
 
   def migrate! do
     block_migrations_dir = Path.join(["priv", "block_repo", "migrations"])
-    migrations_dir = Path.join(["priv", "repo", "migrations"])
+    ops_migrations_dir = Path.join(["priv", "repo", "migrations"])
 
     load_app()
 
     ops_block_repo = OPS.BlockRepo
     ops_block_repo.start_link()
 
-    Ecto.Migrator.run(ops_block_repo, ops_migrations_dir, :up, all: true)
+    Ecto.Migrator.run(ops_block_repo, block_migrations_dir, :up, all: true)
 
     repo = OPS.Repo
     repo.start_link()
 
-    Ecto.Migrator.run(repo, migrations_dir, :up, all: true)
+    Ecto.Migrator.run(repo, ops_migrations_dir, :up, all: true)
 
     System.halt(0)
     :init.stop()
