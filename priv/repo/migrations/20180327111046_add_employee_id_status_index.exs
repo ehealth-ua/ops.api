6120c1876c3cbc7ce97636a9765decd286dc5914
Alex Kovalevych <alexkovalevych@gmail.com>
2018-03-27 15:39:09 +0300

    [patch] add employee_id status index

---
+++ priv/repo/migrations/20180327111046_add_employee_id_status_index.exs
@@ -0,0 +1,9 @@
+defmodule OPS.Repo.Migrations.AddEmployeeIdStatusIndex do
+  @moduledoc false
+
+  use Ecto.Migration
+
+  def change do
+    create(index(:declarations, [:employee_id, :status]))
+  end
+end
