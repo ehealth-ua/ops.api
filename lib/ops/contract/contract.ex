be9b0a634f4ae920fae17439afa5d17f4163898f
Alex Kovalevych <alexkovalevych@gmail.com>
2018-04-23 17:34:44 +0300

    [minor] add contracts schema and migrations

---
+++ lib/ops/contract/contract.ex
@@ -0,0 +1,38 @@
+defmodule OPS.Contracts.Contract do
+  @moduledoc false
+
+  use Ecto.Schema
+  alias Ecto.UUID
+
+  @status_verified "VERIFIED"
+  @status_terminated "TERMINATED"
+
+  def status(:verified), do: @status_verified
+  def status(:terminated), do: @status_terminated
+
+  schema "contracts" do
+    field(:start_date, :naive_datetime)
+    field(:end_date, :naive_datetime)
+    field(:status, :string)
+    field(:contractor_legal_entity_id, UUID)
+    field(:contractor_owner_id, UUID)
+    field(:contractor_base, :string)
+    field(:contractor_payment_details, :map)
+    field(:contractor_rmsp_amount, :integer)
+    field(:external_contractor_flag, :boolean)
+    field(:external_contractors, {:array, :map})
+    field(:nhs_legal_entity_id, UUID)
+    field(:nhs_signer_id, UUID)
+    field(:nhs_payment_method, :string)
+    field(:nhs_signer_base, :string)
+    field(:issue_city, :string)
+    field(:price, :float)
+    field(:contract_number, :string)
+    field(:contract_request_id, UUID)
+    field(:is_active, :boolean)
+    field(:inserted_by, UUID)
+    field(:updated_by, UUID)
+
+    timestamps()
+  end
+end
