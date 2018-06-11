d65bb2ffd360eceda2c3eb07dc336462fe53efbe
Alex Kovalevych <alexkovalevych@gmail.com>
2018-06-11 10:36:33 +0300

    [minor] drop contracts

--- lib/ops/web/router.ex
+++ lib/ops/web/router.ex
@@ -40,10 +40,6 @@ defmodule OPS.Web.Router do
     get("/qualify_medication_requests", MedicationRequestController, :qualify_list)
     get("/prequalify_medication_requests", MedicationRequestController, :prequalify_list)
     get("/latest_block", BlockController, :latest_block)
-
-    resources("/contracts", ContractController, only: [:index, :create, :show])
-    patch("/contracts/actions/suspend", ContractController, :suspend)
-    patch("/contracts/actions/renew", ContractController, :renew)
   end
 
   defp handle_errors(%Plug.Conn{status: 500} = conn, %{kind: kind, reason: reason, stack: stacktrace}) do
