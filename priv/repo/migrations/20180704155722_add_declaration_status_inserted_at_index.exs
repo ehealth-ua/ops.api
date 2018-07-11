a229a3e014a5e818b0ff9271530f670b36476892
Marina Victoria Krasnova <marinakrasnovatr81@gmail.com>
2018-07-11 14:55:03 +0300

    [minor] index concurrently (#105)

--- priv/repo/migrations/20180704155722_add_declaration_status_inserted_at_index.exs
+++ priv/repo/migrations/20180704155722_add_declaration_status_inserted_at_index.exs
@@ -3,6 +3,8 @@ defmodule OPS.Repo.Migrations.AddDeclarationStatusInsertedAtIndex do
 
   @disable_ddl_transaction true
   def change do
-    create(index(:declarations, [:status, :inserted_at], concurrently: true))
+    execute(
+      "CREATE INDEX CONCURRENTLY IF NOT EXISTS declarations_status_inserted_at_index ON declarations(status, inserted_at desc)"
+    )
   end
 end
