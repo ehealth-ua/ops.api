c98f58e40d9d665efb607cf69740f0e115b1eed8
Marina Krasnova <marinakrasnovatr81@gmail.com>
2018-08-22 13:22:58 +0300

    fix index for autotermination (#123)

---
+++ priv/repo/migrations/20180821142022_add_declaration_pending_partial_index.exs
@@ -0,0 +1,16 @@
+defmodule OPS.Repo.Migrations.AddDeclarationPartialPendingIndex do
+  use Ecto.Migration
+
+  @disable_ddl_transaction true
+  def change do
+    execute(
+      "CREATE INDEX CONCURRENTLY IF NOT EXISTS declarations_pending_inserted_at_id_index ON declarations(inserted_at, id) WHERE  status = 'pending_verification'"
+    )
+
+    execute(
+      "CREATE INDEX CONCURRENTLY declarations_legal_entity_status_active ON declarations (legal_entity_id , status) WHERE is_active;"
+    )
+
+    execute("DROP INDEX CONCURRENTLY IF EXISTS declarations_status_inserted_at_index")
+  end
+end
