dcac625851385f15967e25c70c3878895e231e54
Alex Kovalevych <AlexKovalevych@users.noreply.github.com>
2018-12-17 20:21:02 +0200

    add phoenix ecto (#150)

--- apps/core/mix.exs
+++ apps/core/mix.exs
@@ -39,6 +39,7 @@ defmodule Core.MixProject do
       {:ecto, "~> 2.2"},
       {:ex_machina, "~> 2.2"},
       {:ecto_trail, "~> 0.2.4"},
+      {:phoenix_ecto, "~> 3.6"},
       {:scrivener_ecto, "~> 1.3"},
       {:postgrex, ">= 0.0.0"},
       {:ecto_logger_json, git: "https://github.com/edenlabllc/ecto_logger_json.git", branch: "query_params"}
