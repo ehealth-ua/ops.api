2a82505669ddecde5a9d79bae4d2e8d7e191ac70
Marina Krasnova <marinakrasnovatr81@gmail.com>
2019-01-30 11:10:04 +0200

    Git ops (#175)

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Change Log
+
+All notable changes to this project will be documented in this file.
+See [Conventional Commits](Https://conventionalcommits.org) for commit guidelines.
+
+<!-- changelog -->
+
+## [2.4.1](https://github.com/edenlabllc/ds.api/compare/2.4.1...2.4.1) (2019-1-23)
+
+
+
--- mix.exs
+++ mix.exs
@@ -3,8 +3,10 @@ defmodule Ops.MixProject do
 
   use Mix.Project
 
+  @version "2.4.1"
   def project do
     [
+      version: @version,
       apps_path: "apps",
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
@@ -23,7 +25,8 @@ defmodule Ops.MixProject do
       {:distillery, "~> 2.0", runtime: false, override: true},
       {:excoveralls, "~> 0.10.2", only: [:dev, :test]},
       {:credo, "~> 1.0", only: [:dev, :test]},
-      {:ex_doc, "~> 0.19", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+      {:git_ops, "~> 0.6.0", only: [:dev]}
     ]
   end
 end
